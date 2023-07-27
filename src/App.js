b06db52eecfe309085a812ac318e5e78e6cc3ab0
Hamza Mehmood <54894721+dork7@users.noreply.github.com>
2023-07-27 08:12:28 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import './App.css';
+import TreeView from './Tree';
 
 function App() {
+  let data = {
+    lorem: {
+      ipsum2: "dolor sit",
+      amet: {
+        consectetur: "adipiscing",
+        elit: [
+          "duis",
+          "vitae",
+          {
+            semper: "orci"
+          },
+          {
+            est: "sed ornare"
+          },
+          "etiam",
+          ["laoreet", "tincidunt"],
+          ["vestibulum", "ante"]
+        ]
+      },
+      ipsum: "primis"
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+     <TreeView data={data} name='data'/>
     </div>
   );
 }
